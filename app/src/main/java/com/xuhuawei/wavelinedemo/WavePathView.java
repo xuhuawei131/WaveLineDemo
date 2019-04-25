d49f0dfd30c07bd833c6f2df0e1a1d661d54c7b5
xuhuawei <xuhw@knowbox.cn>
2019-04-26 01:34:01 +0800

    Initial commit

---
+++ app/src/main/java/com/xuhuawei/wavelinedemo/WavePathView.java
@@ -0,0 +1,21 @@
+package com.xuhuawei.wavelinedemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class WavePathView extends View {
+
+    public WavePathView(Context context) {
+        super(context);
+    }
+
+    public WavePathView(Context context,  AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WavePathView(Context context,  AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}

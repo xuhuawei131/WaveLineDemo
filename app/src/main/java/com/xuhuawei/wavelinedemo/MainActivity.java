fee3266106e2e1910435f846d74b0859e3419147
xuhuawei <xuhw@knowbox.cn>
2019-04-27 12:16:32 +0800

    项目完成

--- app/src/main/java/com/xuhuawei/wavelinedemo/MainActivity.java
+++ app/src/main/java/com/xuhuawei/wavelinedemo/MainActivity.java
@@ -3,10 +3,14 @@ package com.xuhuawei.wavelinedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, WavePathView.OnWavePathListener {
 
     private WavePathView wavePathView;
+    private TextView btn_undo;
+    private TextView btn_redo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +21,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         View btn_1=findViewById(R.id.btn_1);
         View btn_2=findViewById(R.id.btn_2);
 
+        btn_undo=findViewById(R.id.btn_undo);
+        btn_redo=findViewById(R.id.btn_redo);
+
         btn_0.setOnClickListener(this);
         btn_1.setOnClickListener(this);
         btn_2.setOnClickListener(this);
+        btn_undo.setOnClickListener(this);
+        btn_redo.setOnClickListener(this);
+
+        wavePathView.setOnWavePathListener(this);
     }
 
     @Override
@@ -34,6 +45,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_2:
                 wavePathView.setPathStyle(2);
                 break;
+            case R.id.btn_undo:
+                wavePathView.undoDoolingPaint();
+                break;
+            case R.id.btn_redo:
+                wavePathView.redoDoolingPaint();
+                break;
         }
+    }
+
+    @Override
+    public void onUndoEvent(boolean isCanUndo) {
+        btn_undo.setEnabled(isCanUndo);
+    }
+
+    @Override
+    public void onRedoEvent(boolean isCanRedo) {
+        btn_redo.setEnabled(isCanRedo);
     }
 }
--- app/src/main/java/com/xuhuawei/wavelinedemo/WavePathView.java
+++ app/src/main/java/com/xuhuawei/wavelinedemo/WavePathView.java
@@ -2,8 +2,6 @@ package com.xuhuawei.wavelinedemo;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,9 +18,16 @@ import java.util.List;
 public class WavePathView extends View {
 
     private List<BasePathPaint> arrayList = new ArrayList<>();
+    private List<BasePathPaint> doolingList = new ArrayList<>();
+    private List<BasePathPaint> removingList = new ArrayList<>();
     private BasePathPaint currentInfo;
     private boolean isShowOperateBorader = false;//是否显示操作符和边框
     private int pathStyle = 0;//0优秀、1待提高、2、涂鸦
+    private OnWavePathListener onWavePathListener;
+
+    public void setOnWavePathListener(OnWavePathListener onWavePathListener) {
+        this.onWavePathListener = onWavePathListener;
+    }
 
     public WavePathView(Context context) {
         super(context);
@@ -38,18 +43,24 @@ public class WavePathView extends View {
         super(context, attrs, defStyleAttr);
         init();
     }
+
     private void init() {
 
     }
 
     public void setPathStyle(int pathStyle) {
-        this.pathStyle=pathStyle;
+        this.pathStyle = pathStyle;
     }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         //画以前的路径
         for (BasePathPaint path : arrayList) {
+            path.drawPath(canvas);
+        }
+        //画涂鸦
+        for (BasePathPaint path : doolingList) {
             path.drawPath(canvas);
         }
         if (isShowOperateBorader && currentInfo != null) {
@@ -67,7 +78,7 @@ public class WavePathView extends View {
             case 2:
                 return new DoodlingPathPaint(getContext());
             default:
-                return new ExcellentPaint(getContext());
+                return new DoodlingPathPaint(getContext());
         }
     }
 
@@ -85,7 +96,18 @@ public class WavePathView extends View {
                     isShowOperateBorader = false;
                     currentInfo = getPathPaintByStyle();
                     currentInfo.onEventDown(x, y);
-                    arrayList.add(currentInfo);
+
+                    if (currentInfo instanceof DoodlingPathPaint) {
+                        doolingList.add(currentInfo);
+                        removingList.clear();
+
+                        if (onWavePathListener != null) {
+                            onWavePathListener.onUndoEvent(true);
+                            onWavePathListener.onRedoEvent(false);
+                        }
+                    } else {
+                        arrayList.add(currentInfo);
+                    }
                 }
                 invalidate();
                 break;
@@ -114,6 +136,57 @@ public class WavePathView extends View {
         return true;
     }
 
+    /**
+     * undo操作
+     * @return
+     */
+    public BasePathPaint undoDoolingPaint() {
+        int length=doolingList.size();
+        if (length>0) {
+            BasePathPaint pathPaint= doolingList.remove(length-1);
+            removingList.add(pathPaint);
+            invalidate();
+
+            if (onWavePathListener != null) {
+                onWavePathListener.onRedoEvent(true);
+                length=doolingList.size();
+                if (length>0){
+                    onWavePathListener.onUndoEvent(true);
+                }else{
+                    onWavePathListener.onUndoEvent(false);
+                }
+            }
+
+            return pathPaint;
+        }
+        return null;
+    }
+    /**
+     * redo操作
+     * @return
+     */
+    public BasePathPaint redoDoolingPaint() {
+        int length=removingList.size();
+        if (length>0) {
+            BasePathPaint pathPaint= removingList.remove(length-1);
+            doolingList.add(pathPaint);
+            invalidate();
+
+            if (onWavePathListener != null) {
+                onWavePathListener.onUndoEvent(true);
+                length=removingList.size();
+                if (length>0){
+                    onWavePathListener.onRedoEvent(true);
+                }else{
+                    onWavePathListener.onRedoEvent(false);
+                }
+            }
+
+
+            return pathPaint;
+        }
+        return null;
+    }
     /**
      * 是否点击了控制按钮
      *
@@ -145,4 +218,8 @@ public class WavePathView extends View {
         return false;
     }
 
+    public interface OnWavePathListener {
+        public void onUndoEvent(boolean isCanUndo);
+        public void onRedoEvent(boolean isCanRedo);
+    }
 }
